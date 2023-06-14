663d7298b916dc847df399d0bf0b285a15b73faa
delphine abouab <delphine.abouab@outlook.fr>
2023-06-14 17:07:39 +0200

    form

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+export default function Form() {
+
+  return (
+    <div className="form">
+      <section>
+        <input type="text" />
+        <input type="text" />
+      </section>
+      <input type="submit" id="submit" value="Get a new meme image 🖼️"/>
+    </div>
+  )
+}
