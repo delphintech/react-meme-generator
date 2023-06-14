8d3abfd742be8f229f7227b9a1aa7bb5fddd951e
delphine abouab <delphine.abouab@outlook.fr>
2023-06-14 15:09:22 +0200

    new files

---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+
+  return (
+    <div className="navbar">
+      <section className='nav-left'>
+        <img src="src/assets/images/troll.png" alt="Troll face logo" />
+        <h3>Meme Generator</h3>
+      </section>
+      <p>React Course - Project 3</p>
+    </div>
+  )
+}
