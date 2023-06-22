737a1ea6d7060aae337ecf2bd76b65045f991571
delphine abouab <delphine.abouab@outlook.fr>
2023-06-22 17:12:41 +0200

    Form handling refracto

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,7 @@ import Meme from "./Meme"
 export default function Form() {
   const [meme, setMeme] = React.useState({topText: 0, bottomText: 0, img: 0})
 
-  const [allMemes, setAllMemes] = React.useState(data.data.memes)
+  const [allMemes] = React.useState(data.data.memes)
 
   function handleClick () {
     const rand = Math.floor(Math.random() * allMemes.length)
@@ -13,22 +13,17 @@ export default function Form() {
     setMeme(prevMeme => ({...prevMeme, img: randMeme.url}))
   }
 
-  function topForm(event) {
-    const text = event.target.value
-    setMeme(prevMeme => ({...prevMeme, topText: text}))
+  function handleForm(event) {
+    setMeme(prevMeme => ({...prevMeme, [event.target.name]: event.target.value}))
   }
 
-  function bottomForm() {
-    const text = event.target.value
-    setMeme(prevMeme => ({...prevMeme, bottomText: text}))
-  }
 
   return (
     <div className="content">
       <div className="form">
         <section>
-          <input type="text" placeholder=" Top text" onChange={topForm} />
-          <input type="text" placeholder=" Bottom text" onChange={bottomForm} />
+          <input type="text" placeholder=" Top text" onChange={handleForm} name="topText" />
+          <input type="text" placeholder=" Bottom text" onChange={handleForm} name="bottomText" />
         </section>
         <input type="submit" id="submit" value="Get a new meme image 🖼️" onClick={handleClick} />
       </div>
